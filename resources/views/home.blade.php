7ecca1373233aa95de250cda73febd7f43361c8f
sithijinia <sithi.jinia@gmail.com>
2018-01-18 12:54:23 +0600

    start

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+
+{{--<a href="{{  route('/') }}">Home</a>--}}
+{{--<a href="{{ route('/about') }}">About</a>--}}
+
+
+{{--<a href="{!!  route('/') !!}">Home</a>--}}
+{{--<a href="{!! route('/about') !!}">About</a>--}}
+
+
+
+{{--<h1>This is a new view file for Laravel.</h1>--}}
+{{--<h1>{{ $name }}</h1>--}}
+{{--<h1>{{ $age }}</h1>--}}
